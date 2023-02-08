f88604afe1ecde34c4e2401b6ffb7d9228dd519c
dom-inic <dominicnyambane22@gmail.com>
2023-02-08 23:59:30 +0300

    list implementation

--- listADT.js
+++ listADT.js
@@ -1,2 +1,21 @@
-// An implementation of a List ADT 
+// An implementation of a List ADT
 
+function List() {
+  this.listSize = 0;
+  this.pos = 0;
+  this.dataStore = [];
+  this.toString = toString;
+  this.insert = insert;
+  this.append = append;
+  this.remove = remove;
+  this.front = front;
+  this.end = end;
+  this.prev = prev;
+  this.next = next;
+  this.length = length;
+  this.currPos = currPos;
+  this.moveTo = moveTo;
+  this.getElement = getElement;
+  this.length = length;
+  this.contains = contains; // initializes an empty array to store list elements this.clear = clear;
+}
