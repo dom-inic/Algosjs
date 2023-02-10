3dae56abeb980a52c1d19fd9a6ce10ff3531ddcc
dom-inic <dominicnyambane22@gmail.com>
2023-02-10 23:47:27 +0300

    queues

---
+++ queues.js
@@ -0,0 +1,11 @@
+class Queue{
+    constructor(){
+        this.datastore = [];
+        this.bottom = 0;
+        this.push = push;
+        this.pop = pop;
+        this.clear = clear;
+    }
+
+
+}
