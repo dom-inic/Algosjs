f6199f6d77afe072d6cb3d48232d091b66c7b9c2
dom-inic <dominicnyambane22@gmail.com>
2023-02-12 23:59:06 +0300

    queue methods

--- queues.js
+++ queues.js
@@ -1,11 +1,27 @@
-class Queue{
-    constructor(){
-        this.datastore = [];
-        this.bottom = 0;
-        this.push = push;
-        this.pop = pop;
-        this.clear = clear;
-    }
+class Queue {
+  constructor() {
+    this.datastore = [];
+    this.enqueue = enqueue;
+    this.dequeue = dequeue;
+    this.front = front;
+    this.back = back;
+    this.toString = toString;
+    this.empty = empty;
+  }
 
+  enqueue(element) {
+    this.dataStore.push(element);
+  }
 
+  dequeue() {
+    return this.dataStore.shift();
+  }
+
+  front() {
+    return this.dataStore[0];
+  }
+
+  back() {
+    return this.dataStore[this.dataStore.length - 1];
+  }
 }
